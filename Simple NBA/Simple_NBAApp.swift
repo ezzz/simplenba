2bdfab6e39696670edb7db9fbe5ba08ebd699466
Bruno <ezzz75@gmail.com>
2022-11-02 12:20:05 +0100

    Initial Commit

---
+++ Simple NBA/Simple_NBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_NBAApp.swift
+//  Simple NBA
+//
+//  Created by Bruno ARENE on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Simple_NBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
